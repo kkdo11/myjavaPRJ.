3ca8678e90bf1ebfffc56b15578d66bc533f2a3e
kdw03 <kdw030612@gmail.com>
2022-03-18 22:03:07 +0900

    20220318

---
+++ src/main/java/Ex03_06.java
@@ -0,0 +1,9 @@
+public class Ex03_06 {
+    public static void main(String[] args) {
+        System.out.printf("\n줄바꿈 \n연습\n" );
+        System.out.printf("\t탭키연습\n");
+        System.out.printf("\r이것을 \r덮어씁니다\n");
+        System.out.printf("highlight \"this\"letter\n");
+        System.out.printf("\\\\\\\\");
+    }
+}
