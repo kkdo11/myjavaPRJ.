21b805487a1232d01e7022e33da363546cd82227
kdw03 <kdw030612@gmail.com>
2022-04-01 08:59:11 +0900

    20220401

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello my first java coding");
+        Scanner s =new Scanner(System.in);
+
+        int n;
+        n=s.nextInt();
+        System.out.printf("%d",n);
+        
 
     }
 }
