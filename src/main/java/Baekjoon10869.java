397dc43ceca78d330c0277121378504f38af5209
kdw03 <kdw030612@gmail.com>
2022-03-31 17:14:19 +0900

    20220330

---
+++ src/main/java/Baekjoon10869.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Baekjoon10869 {
+    public static void main(String[] args) {
+         Scanner s=new Scanner(System.in);
+
+         int a, b;
+         a=s.nextInt();
+         b=s.nextInt();
+
+        System.out.printf("a+b");
+        System.out.printf("a-b");
+        System.out.printf("a*b");
+        System.out.printf("a/b");
+        System.out.printf("a%b");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/Baekjoon1330.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Baekjoon1330 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a,b;
+
+        a=s.nextInt();
+        b=s.nextInt();
+
+        if(a>b)
+            System.out.printf(">");
+
+        if(a<b)
+            System.out.printf("<");
+
+        if(a==b)
+            System.out.printf("=");
+
+
+
+
+
+    }
+}
