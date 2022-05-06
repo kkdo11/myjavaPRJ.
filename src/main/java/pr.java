821dd487f8b9c2b921a2c9eb3c9e181da5da46df
kdw03 <kdw030612@gmail.com>
2022-05-06 20:12:04 +0900

    20220503

---
+++ src/main/java/pr.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pr {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int aa[]=new int[n];
+
+        
+    }
+}
