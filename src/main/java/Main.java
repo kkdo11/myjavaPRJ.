94bbae87e2f6004e163d6fcf3b6e999aadaca139
kdw03 <kdw030612@gmail.com>
2022-05-16 23:40:35 +0900

    20220512

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int []arr = new int[3];
-
-        for(int i=0; i<3; i++) {
-            arr[i]= scanner.nextInt();
-            if(arr[i]%2 ==0) {
-                System.out.println(arr[i]);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            if (a != 0) {
+                System.out.println(a);
+            } else {
+                break;
             }
         }
-
-
     }
 }
+
+
