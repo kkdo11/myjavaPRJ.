5eb028cad688596159be55d45942681809ba193d
kdw03 <kdw030612@gmail.com>
2022-05-17 23:20:13 +0900

    20220512

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            int a = sc.nextInt();
-            if (a != 0) {
-                System.out.println(a);
-            } else {
-                break;
+        String a = sc.next();
+        char[] li = a.toCharArray();
+        for (int i = 0; i < li.length; i++) {
+            System.out.print("[" + li[i]);
+            for (int j = li.length - 1; j > i; j--) {
+                System.out.print("0");
             }
+            System.out.print("]\n");
         }
     }
 }
-
 
