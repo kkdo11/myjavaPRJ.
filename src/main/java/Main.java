51338c135c4c762039dc9bbbca542de7513e4ae5
kdw03 <kdw030612@gmail.com>
2022-05-18 00:05:07 +0900

    20220518

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,9 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        char[] li = a.toCharArray();
-        for (int i = 0; i < li.length; i++) {
-            System.out.print("[" + li[i]);
-            for (int j = li.length - 1; j > i; j--) {
-                System.out.print("0");
-            }
-            System.out.print("]\n");
-        }
+        double num = sc.nextDouble();
+        String str = String.format("%.11f", num);
+        System.out.print(str);
     }
 }
-
