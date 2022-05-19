32d39cd3a3ce7f941816a3e9b2256da8972534a9
kdw03 <kdw030612@gmail.com>
2022-05-19 20:04:01 +0900

    20220519

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double num = sc.nextDouble();
-        String str = String.format("%.11f", num);
-        System.out.print(str);
+        Scanner in = new Scanner(System.in);
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+        int counts[] = new int[10];
+        int number = a * b * c;
+        while (number > 0) {
+            counts[number % 10]++;
+            number /= 10;
+        }
+
+        for (int i = 0; i < counts.length; ++i) {
+            System.out.println(counts[i]);
+        }
+
+
     }
 }
