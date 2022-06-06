ccfe6227ccc313a0c84c46fca95e852fc3cc4a7d
kdw03 <kdw030612@gmail.com>
2022-06-06 15:03:26 +0900

    20220606

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,38 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int[] arr;
-
-        int testcase = in.nextInt();
-
-        for(int i = 0 ; i < testcase ; i++) {
-
-            int N = in.nextInt();
-            arr = new int[N];
-
-            double sum = 0;
-
-            for(int j = 0 ; j < N ; j++) {
-                int val = in.nextInt();
-                arr[j] = val;
-                sum += val;
-            }
-
-            double mean = (sum / N) ;
-            double count = 0;
-
-
-            for(int j = 0 ; j < N ; j++) {
-                if(arr[j] > mean) {
-                    count++;
-                }
-            }
-
-            System.out.printf("%.3f%%\n",(count/N)*100);
-
-
+        int a = in.nextInt();
+        for (int i = a; i >=0; i--) {
+            a--;
+            System.out.println(a);
         }
-        in.close();
     }
-
 }
