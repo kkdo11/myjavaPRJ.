db295d2eca6d218c6187b84e0f411efd37f357bd
kdw03 <kdw030612@gmail.com>
2022-06-13 19:53:43 +0900

    20220606

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        char n = in.next().charAt(0);
-        char a = 'a';
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int r=s.nextInt();
+        int n=s.nextInt();
 
-        while (a <= n) {
-            System.out.print(a + " ");
-            a+=1;
-        }
+        System.out.printf("%d", (long)(a*Math.pow(r, n-1)));
+        //>Math.pow() 메소드는 입력값과 출력값은 모두 double형이며 Math.pow(대상숫자,지수)를 넣음
     }
 }
