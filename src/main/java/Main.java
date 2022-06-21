e75bb5d067e34f9a1d4ce20dca876c6c08b9dfc1
kdw03 <kdw030612@gmail.com>
2022-06-21 18:08:56 +0900

    202206021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,26 @@
-class Test {
-    long sum(int[] a) {
-        long sum = 0;	// a 배열 정수 합 변수
+import java.util.Arrays;
 
-        for(int i = 0; i < a.length; i++) {
-            sum += a[i];
+public class Main{
+    public static void main(String[] args) {
+        int arr1[]=new int[10];
+        int arr2[]=new int[10];
+        int arr3[]={100,95,80,70,65};
+        char ch[]= {'a','b','c','d'};
+
+        for (int i=0;i< arr1.length;i++){
+            arr1[i]=i+1;
         }
-        return sum;
+        for (int i=0; i<arr2.length; i++){
+            arr2[i]=(int)(Math.random()*10)+1;
+        }
+
+        for (int i=0; i<arr3.length; i++){
+            System.out.print(arr3[i]+",");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr3));
+        System.out.println(ch);
     }
 }
