7fb8ff878d8efc6cc7a494edff35f2085eb8c286
kdw03 <kdw030612@gmail.com>
2022-06-27 20:39:31 +0900

    202206021

--- src/main/java/BJ2562.java
+++ src/main/java/BJ2562.java
@@ -2,25 +2,15 @@ import java.util.Scanner;
 
 public class BJ2562 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
-                in.nextInt(), in.nextInt(), in.nextInt(),
-                in.nextInt(), in.nextInt(), in.nextInt() };
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
 
-
-
-
-        int max = 0;
-        int index = 0;
-        int value=0;
-
-        for(int i = 0 ; i < 9 ; i++) {
-            if(value > max) {
-                max = value;
-                index = (i + 1);
-            }
+        if (a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
         }
-        System.out.print(max + "\n" + index);
 
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,13 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        int arr1[]=new int[10];
-        int arr2[]=new int[10];
-        int arr3[]={100,95,80,70,65};
-        char ch[]= {'a','b','c','d'};
+        Scanner s = new Scanner(System.in);
+        float a=s.nextFloat();
+        if (a>=50&&a<=60)
+            System.out.println("win");
+        else
+            System.out.println("lose");
 
-        for (int i=0;i< arr1.length;i++){
-            arr1[i]=i+1;
-        }
-        for (int i=0; i<arr2.length; i++){
-            arr2[i]=(int)(Math.random()*10)+1;
-        }
-
-        for (int i=0; i<arr3.length; i++){
-            System.out.print(arr3[i]+",");
-        }
-        System.out.println();
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(arr3));
-        System.out.println(ch);
     }
 }
