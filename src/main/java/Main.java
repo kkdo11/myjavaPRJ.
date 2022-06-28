6d41d45fdfa04007681c439e48e53d7370be2459
kdw03 <kdw030612@gmail.com>
2022-06-28 22:12:29 +0900

    202206021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        float a=s.nextFloat();
-        if (a>=50&&a<=60)
-            System.out.println("win");
-        else
-            System.out.println("lose");
+        Scanner s=new Scanner(System.in);
 
+        int a,b,c;
+        a=s.nextInt();
+        b=s.nextInt();
+        c=s.nextInt();
+
+        if (a<=170||b<=170||c<=170)
+            System.out.println("CRASH");
+        else
+            System.out.println("PASS");
     }
 }
