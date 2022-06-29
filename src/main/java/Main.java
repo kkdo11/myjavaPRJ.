2fbcedf43eebde258f81e9abee4fb3556f15a783
kdw03 <kdw030612@gmail.com>
2022-06-29 16:30:45 +0900

    202206021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        for(int i = a; i < 90; i+=5){
-            b++;
-        }
-        System.out.print(b);
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int sum=a+b;
+        if (a%2==0)
+            System.out.printf("짝수");
+        else
+            System.out.printf("홀수");
+        System.out.printf("+");
+        if (b%2==0)
+            System.out.printf("짝수");
+        else
+            System.out.printf("홀수");
+        System.out.printf("=");
+        if(sum%2==0)
+            System.out.printf("짝수");
+        else
+            System.out.printf("홀수");
+
     }
 }
