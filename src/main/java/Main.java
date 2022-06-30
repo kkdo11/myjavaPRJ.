91f5a4cd5411ad83f084cbebdfac49a7ed98293d
kdw03 <kdw030612@gmail.com>
2022-06-30 18:21:02 +0900

    202206021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,13 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner s= new Scanner(System.in);
-        int a=s.nextInt();
-        int b=s.nextInt();
-        int sum=a+b;
-        if (a%2==0)
-            System.out.printf("짝수");
-        else
-            System.out.printf("홀수");
-        System.out.printf("+");
-        if (b%2==0)
-            System.out.printf("짝수");
-        else
-            System.out.printf("홀수");
-        System.out.printf("=");
-        if(sum%2==0)
-            System.out.printf("짝수");
-        else
-            System.out.printf("홀수");
+        Scanner s = new Scanner(System.in);
+        int arr[] = {s.nextInt(), s.nextInt(), s.nextInt()};
+        Arrays.sort(arr);   //오름차순 정렬은 Array.sort(베열이름);
 
+        for (int i=0; i<arr.length; i++)
+            System.out.print(arr[i]+" ");
     }
 }
