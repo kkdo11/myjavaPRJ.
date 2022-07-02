19f1e6bb6fc05c2b5b98d98194413b8cb31bb0f4
kdw03 <kdw030612@gmail.com>
2022-07-02 22:57:43 +0900

    20220702

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner s= new Scanner(System.in);
-        int arr[]=new int[9];
-        for (int i=0; i< arr.length; i++)
-            arr[i]=s.nextInt();
-
-        int max=0;
-        int count=0;
-        for (int i=0; i<arr.length; i++)
-            if (arr[i]>max) {
-                max = arr[i];
-                count=i+1;
-            }
-        System.out.println(max);
-        System.out.println(count);
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        if (a<10)
+            System.out.println("small");
+        else
+            System.out.printf("big");
     }
-    }
+}
 
