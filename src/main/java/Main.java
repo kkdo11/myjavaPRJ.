04bc2db6206161e3f074a2ed152a0b041b2e186f
kdw03 <kdw030612@gmail.com>
2022-07-04 20:42:37 +0900

    20220702

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner s= new Scanner(System.in);
-
-        int a=s.nextInt();
-
-        if (a%7==0)
-            System.out.println("multiple");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        if ((a >= 30 && a <= 40) || (a >= 60 && a <= 70))
+            System.out.println("win");
         else
-            System.out.println("not multiple");
+            System.out.printf("lose");
     }
 }
-
