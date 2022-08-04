d0e0b2e2319587a74e4dcf2ec1644ef0bd00da5b
kdw03 <kdw030612@gmail.com>
2022-08-04 16:57:17 +0900

    20220804

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner s= new Scanner(System.in);
-        int y=s.nextInt();
-        int m=s.nextInt();
-        int d=s.nextInt();
-        int a=y+m+d;
+  Scanner s= new Scanner(System.in);
+  int a=s.nextInt();
+  if((a%400==0)||(a%4==0&&a%100!=0))
+      System.out.println("Leap");
+  else
+      System.out.printf("Normal");
 
-        if (a/100%2==0)
-            System.out.println("대박");
-        else
-            System.out.printf("그럭저럭");
     }
 }
