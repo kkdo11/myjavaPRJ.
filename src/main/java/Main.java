689e3f97a4bef5abd0df367a4637e2c437acba70
kdw03 <kdw030612@gmail.com>
2022-08-05 15:03:23 +0900

    20220804

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
   Scanner s= new Scanner(System.in);
-  int a=s.nextInt();
-  if((a%400==0)||(a%4==0&&a%100!=0))
-      System.out.println("Leap");
-  else
-      System.out.printf("Normal");
+ int aa[]={s.nextInt(),s.nextInt(),s.nextInt()};
+        Arrays.sort(aa);
+        System.out.print(aa[1]);
 
     }
 }
