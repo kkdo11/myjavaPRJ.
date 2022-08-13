d403760b4b8c27ef44c62de8885058118ccaf0fb
kdw03 <kdw030612@gmail.com>
2022-08-13 22:28:02 +0900

    20220813

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,19 @@
     public class Main {
         public static void main(String[] args){
             Scanner s= new Scanner(System.in);
-            int a=s.nextInt();
 
 
-            if(a<0)
-                System.out.printf("음수");
-            else if (a>0)
-                System.out.printf("양수");
-            else
-                System.out.printf("0");
+               int a=s.nextInt();
+               if (a % 10 == 1 && a != 11)
+                   System.out.println(a + "st");
+               else if (a % 10 == 2 && a != 12)
+                   System.out.println(a + "nd");
+               else if (a % 10 == 3 && a != 13)
+                   System.out.println(a + "rd");
+               else if (a==11||a==12||a==13)
+                   System.out.println(a + "th");
+               else
+                   System.out.println(a+"th");
 
 
         }
