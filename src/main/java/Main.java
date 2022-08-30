034fd0a17f653ce4093fa2db113c9f8dd575a3c7
kdw03 <kdw030612@gmail.com>
2022-08-30 22:05:29 +0900

    20220830

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,40 @@
-  import java.util.Scanner;
+import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args){
             Scanner s=new Scanner(System.in);
             int a= s.nextInt();
             int b= s.nextInt();
-            int c=b/a;
-            int d=a/b;
+            int c=0;
+            int d=0;
 
-            if (b%a==0)
-                System.out.printf("%d*%d=%d",a,c,b);
-            else if (a%b==0)
-                System.out.printf("%d*%d=%d",b,d,a);
+            if(a==1){
+                c=400;}
+            else if(a==2){
+                c=340;}
+            else if(a==3){
+                c=170;}
+            else if(a==4){
+                c=100;}
+            else if(a==4){
+                c=70;}
+
+            if(b==1){
+                d=400;}
+            else if(b==2){
+                d=340;}
+            else if(b==3){
+                d=170;}
+            else if(b==4){
+                d=100;}
+            else if(b==4){
+                d=70;}
+
+            if (c+d>500)
+                System.out.println("angry");
             else
-                System.out.println("none");
+                System.out.printf("no angry");
+
+
         }
     }
