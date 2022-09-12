6ea9f983849f6141289473bc6e6875872ed18c37
kdw03 <kdw030612@gmail.com>
2022-09-12 16:48:29 +0900

    20220912

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,18 @@
 import java.util.Scanner;
 
-    public class Main {
+public class Main {
         public static void main(String[] args){
-            Scanner s=new Scanner(System.in);
-            int a= s.nextInt();
-            int b= s.nextInt();
-            int c=0;
-            int d=0;
+            Scanner s= new Scanner(System.in);
+            int a,b,c;
+            a=s.nextInt();
+            b=s.nextInt();
+            c=s.nextInt();
 
-            if(a==1){
-                c=400;}
-            else if(a==2){
-                c=340;}
-            else if(a==3){
-                c=170;}
-            else if(a==4){
-                c=100;}
-            else if(a==4){
-                c=70;}
-
-            if(b==1){
-                d=400;}
-            else if(b==2){
-                d=340;}
-            else if(b==3){
-                d=170;}
-            else if(b==4){
-                d=100;}
-            else if(b==4){
-                d=70;}
-
-            if (c+d>500)
-                System.out.println("angry");
-            else
-                System.out.printf("no angry");
-
-
+            if (a+b>c&&b+c>a&&a+c>b){
+                System.out.printf("yes");
+            }
+            else {
+                System.out.printf("no");
+            }
         }
     }
