8559102f5c0b5d3325b6e3b2b466447f2a1e84a2
kdw03 <kdw030612@gmail.com>
2022-09-24 14:46:09 +0900

    20220922

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,15 @@ public class Main{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |");
+        int A = in.nextInt();
+        String B = in.next();
 
+        in.close();
+
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
+        System.out.println(A * Integer.parseInt(B));
 
     }
 
