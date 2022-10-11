0f439ae389eea01b36a4020e3fc00635a3c79f72
kdw03 <kdw030612@gmail.com>
2022-10-11 23:12:07 +0900

    20221011

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
-
-public class Main{
-
+public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        int A = in.nextInt();
-        String B = in.next();
-
-        in.close();
-
-        System.out.println(A * (B.charAt(2) - '0'));
-        System.out.println(A * (B.charAt(1) - '0'));
-        System.out.println(A * (B.charAt(0) - '0'));
-        System.out.println(A * Integer.parseInt(B));
+        class Test{
+            long sum(int[] a){
+                long sum=0;
+            for (int i=0; i<a.length; i++){
+                sum+=a[i];
+            }
+            return sum;
+            }
+        }
 
     }
+}
+class Test {
+    long sum(int[] a) {
+        long sum = 0;	// a 배열 정수 합 변수
 
+        for(int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
 }
